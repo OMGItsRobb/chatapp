0ade8b848fe1339ea32100fe964bca89876bb437
Nolan <ntronowicz@gmail.com>
2021-10-30 13:21:50 -0400

    added signin page and signin with facebook and google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyCly7Qb3EFlCSk7T111W66Vd8CaoLorQfs',
@@ -10,3 +12,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,80 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import { Container, Row, Grid, Col, Panel, Button } from 'rsuite';
+import { FaGoogle, FaFacebookSquare } from 'react-icons/fa';
+import { auth, database } from '../misc/firebase';
 
 function SignIn() {
-  return <div>Sign In</div>;
+  const signInWithProvider = async provider => {
+    try {
+      const result = await auth.signInWithPopup(provider);
+      console.log(result);
+
+      if (result.additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${result.user.uid}`).set({
+          name: result.user.displayName,
+          email: result.additionalUserInfo.profile.email,
+          verifiedEmail: result.additionalUserInfo.profile.verified_email,
+          id: result.additionalUserInfo.profile.id,
+          providerType: result.additionalUserInfo.providerId,
+          createdAtUNIX: firebase.database.ServerValue.TIMESTAMP,
+          createdAt: `${new Date().toLocaleString('en-US', {
+            timeZone: 'UTC',
+          })} (UTC)`,
+        });
+      }
+      console.log('Success!');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onFacebookSignin = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+
+  const onGoogleSignin = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat!</h2>
+                <p>Sick-awesome chat platform for idiots</p>
+              </div>
+              <div className="mt-3">
+                <Button
+                  color="blue"
+                  block
+                  appearance="primary"
+                  onClick={onFacebookSignin}
+                >
+                  <FaFacebookSquare />
+                  <p> </p>
+                  Facebook Sign-in
+                </Button>
+                <Button
+                  color="green"
+                  block
+                  appearance="primary"
+                  onClick={onGoogleSignin}
+                >
+                  <FaGoogle />
+                  <p></p>
+                  Google Sign-in
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 }
 
 export default SignIn;
